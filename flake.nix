b0311c99d51331bb847b53d56dc7bac2834de0e2
Mateusz Jakobsche <92634489+mjakobsche@users.noreply.github.com>
2024-10-07 10:39:21 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
 
       toDEB = drv: nix-utils.bundlers.deb {inherit system; program=program drv;};
 
-      showPackage = drv: builtins.trace "package: " (toString drv);
+      showPackage = drv: builtins.trace "package: ${toString drv}" (toDEB drv);
 
       toDockerImage = {...}@drv:
         (nixpkgs.legacyPackages.${system}.dockerTools.buildLayeredImage {
